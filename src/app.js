71eb6fd38329e84e171511d3b4992ba97407ba9a
Kubra <cskubra@gmail.com>
2022-09-20 23:39:12 +0300

    Extensions install

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"
 console.log("<ul>")
 for(let i = 0;i<konutKredileri.length;i++){
     console.log("<li>"+konutKredileri[i]+"</li>")
+    
 }
 console.log("</ul>")
 
